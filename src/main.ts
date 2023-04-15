2eacb6251a2503fb8c275d3dc60e7e046628044c
Jesus Paguay <desarrollador3@comboplay.co>
2023-04-14 21:40:32 -0500

    all conections

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://chat-soreje.netlify.app/auth/login'],
-    methods: ['GET','POST']
+    origin: ['*'],
+    methods: ['*'],
   });
   await app.listen(3000);
 }
